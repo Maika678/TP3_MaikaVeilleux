c1190ccbf430a168854461aa794db113af017087
Maika678 <1776430@cegepmontpetit.ca>
2025-12-02 12:42:38 -0500

    chiffrement NAS

--- 3U4-TP3-1.0.0.0/DonneesSecurite.cs
+++ 3U4-TP3-1.0.0.0/DonneesSecurite.cs
@@ -2,31 +2,47 @@
 // https://www.mscs.dal.ca/~selinger/md5collision/
 
 using System;
+using System.Text;
 using BCrypt.Net;
+using System.IO;
+using System.Security.Cryptography;
 namespace consoleApp
 {
     class DonneesSecurite
     {
+        private static readonly byte[] Key = Encoding.UTF8.GetBytes("12345678901234561234567890123456"); // 32 bytes AES-256
+        private static readonly byte[] IV = Encoding.UTF8.GetBytes("1234567890123456"); // 16 bytes
+
         public static string Encrypter(string input)
         {
-            string result = "";
-            foreach (char c in input)
-            {
-                char t = (char)(c * 2);
-                result += t;
-            }
-            return result;
+            using Aes aes = Aes.Create();
+            aes.Key = Key;
+            aes.IV = IV;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform encryptor = aes.CreateEncryptor();
+
+            byte[] plainBytes = Encoding.UTF8.GetBytes(input);
+            byte[] encryptedBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+
+            return Convert.ToBase64String(encryptedBytes);
         }
 
         public static string Decrypter(string input)
         {
-            string result = "";
-            foreach (char c in input)
-            {
-                char t = (char)(c / 2);
-                result += t;
-            }
-            return result;
+            using Aes aes = Aes.Create();
+            aes.Key = Key;
+            aes.IV = IV;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform decryptor = aes.CreateDecryptor();
+
+            byte[] cipherBytes = Convert.FromBase64String(input);
+            byte[] plainBytes = decryptor.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+
+            return Encoding.UTF8.GetString(plainBytes);
         }
 
         // fonctions permettant le hachage des mots de passe
